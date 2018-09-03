f8c84295f226056a5f7906d85e19160a206e0fcc
vagrant <vagrant@homestead>
2018-09-03 13:43:25 +0000

    Keep track of user last visited time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,17 @@ class Kernel extends ConsoleKernel
         $schedule->command('larabbs:calculate-active-user')->hourly();
     }
 
+    protected function schedule(Schedule $schedule)
+    {
+        // 用户最后登陆时间记录
+        // $schedule->command('inspire')
+        //          ->hourly();
+        // 每隔一个小时执行一遍
+        $schedule->command('larabbs:calculate-active-user')->hourly();
+        // 每日零时执行一次
+        $schedule->command('larabbs:sync-user-actived-at')->dailyAt('00:00');
+    }
+
     /**
      * Register the commands for the application.
      *
