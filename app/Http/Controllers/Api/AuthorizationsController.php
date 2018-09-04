83023026b4567cc6e3ddd99519a0e41f2e7371cc
vagrant <vagrant@homestead>
2018-09-04 07:53:39 +0000

    add jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
+use App\Http\Requests\Api\AuthorizationRequest;
+use Auth;
 
 class AuthorizationsController extends Controller
 {
@@ -29,6 +31,7 @@ class AuthorizationsController extends Controller
             }
 
             $oauthUser = $driver->userFromToken($token);
+            dd($oauthUser);
         } catch (\Exception $e) {
             return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
@@ -50,12 +53,64 @@ class AuthorizationsController extends Controller
                         'avatar' => $oauthUser->getAvatar(),
                         'weixin_openid' => $oauthUser->getId(),
                         'weixin_unionid' => $unionid,
+                        // 只有在用户将公众号绑定到微信开放平台帐号后，才会出现 unionid 字段。
+                        // 'weixin_unionid' => $oauthUser->offsetGet('unionid'),
                     ]);
                 }
 
                 break;
         }
 
-        return $this->response->array(['token' => $user->id]);
+        // return $this->response->array(['token' => $user->id]);
+
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        // return $this->response->array([
+        //     'access_token' => $token,
+        //     'token_type' => 'Bearer',
+        //     'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        // ])->setStatusCode(201);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
     }
 }
+
+
+
+
